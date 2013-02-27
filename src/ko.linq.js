b526c28991841c86269231a7ca3c205fd1a24e81
ozbe <1372945+ozbe@users.noreply.github.com>
2013-02-26 19:20:34 -0600

    JSHint compliant

--- src/ko.linq.js
+++ src/ko.linq.js
@@ -1,21 +1,25 @@
-(function (Enumerable, ko) {      
-    for (propertyName in Enumerable.prototype) {
+(function (Enumerable, ko) {    
+	"use strict";  
+	
+	var mediator = function (methodName) {
+		return function () {
+			var enumerable = Enumerable.From(this());             
+			return enumerable[methodName].apply(enumerable, arguments);
+		};
+	};
+	
+    for (var propertyName in Enumerable.prototype) {
         
         if (!Enumerable.prototype[propertyName].apply ||
             ko.observableArray.fn[propertyName]) {
              continue;   
         }
         
-        ko.observableArray.fn[propertyName] = (function (methodName) {
-            return function () {
-                var enumerable = Enumerable.From(this())                
-                return enumerable[methodName].apply(enumerable, arguments);
-            };
-        })(propertyName);
+        ko.observableArray.fn[propertyName] = mediator(propertyName);
     }
     
     // Add ToObservableArray
     Enumerable.prototype.ToObservableArray = function() {
         return ko.observableArray(this.ToArray());
-    }
+    };
 })(Enumerable, ko);
